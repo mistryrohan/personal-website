c7c3a9bff98097df622513a35ade54f43376626d
Mistryr222 <mistryr222@gmail.com>
2025-01-06 19:02:06 -0500

    favicon and edits

--- src/components/About.js
+++ src/components/About.js
@@ -4,9 +4,15 @@ const About = () => {
   return (
     <section id="about" className="mt-10 ml-16 mr-44">
       <p className='leading-relaxed mb-3'>
-      Welcome to my website! I am a developer who loves to learn about the latest in software and data and integrate that into solutions and projects. I have had an opportunity to apply my skills and learn best practices from my internship at BlueCat, hackathons, and personal projects. 
+      Welcome to my website! I am a developer who loves to learn about the latest in software and data 
+      and integrate that into solutions and projects. I have had an opportunity to apply 
+      my skills and learn best practices from my internship at BlueCat, hackathons, and personal projects. 
       </p>
-      <p>Some of my personal interests are sports such as soccer and basketball, music and concerts, and finding ways to get a deeper understanding of the world around me.</p>
+      
+      <p>Some of my personal interests are sports such as soccer and basketball, 
+        music and concerts, 
+        and finding ways to get a deeper understanding of the world around me.
+      </p>
     </section>
   )
 }
