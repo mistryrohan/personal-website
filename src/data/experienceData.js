cf1560d8f2a8c21d54d98a8c5124cde0a9cb5b3f
Mistryr222 <mistryr222@gmail.com>
2025-05-04 21:25:12 -0400

    edit information

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -5,8 +5,8 @@ const experienceData = [
       company: "BlueCat",
       dateRange: "May 2024 — Present",
       location: "Toronto, ON",
-      description: `Worked across different teams on both front-end and back-end overhauls. Used TypeScript and React to implement new functionality, optimize components, and fix critical defects. Developed REST API endpoints in Java, resolved back-end defects, and wrote system tests using JUnit to enable real-time front-end updates and reduce test failures. Wrote and maintained Jest test suites to increase coverage by 30%. Supported team onboardings for the new UI by establishing best practices and providing guidance to ensure smooth transitions for engineers.`,
-      skills: ["TypeScript", "React", "Java", "JUnit", "Jest", "Selenium"],
+      description: `I contributed to a TypeScript and React front-end overhaul, implementing new functionality, optimizing components, and fixed critical defects to accelerate a major product release. Also, I developed REST API endpoints in Java, resolved back-end defects, fixed PostgreSQL queries, and wrote system tests using JUnit to enable real-time front-end updates and reduce test failures by 15%. Additionally, I wrote and maintained Jest test suites to increase coverage by 30% and ensure code quality was maintained. I also supported team onboardings for the new UI, performed code reviews, and provided guidance to ensure smooth transitions for new engineers.`,
+      skills: ["TypeScript", "React", "Java", "JUnit", "Jest", "PostgreSQL", "Selenium"],
     },
     {
       id: 2,
@@ -14,7 +14,7 @@ const experienceData = [
       company: "UNICEF",
       dateRange: "September 2023 — December 2023",
       location: "Toronto, ON",
-      description: `Developed an open-source web application using Python, JavaScript, and React to replace a command line proof-of-concept in an agile team for over 100 UNICEF offices worldwide to save licensing costs. Implemented a cloud-based version control feature using Firebase and Flask to allow large teams to effectively manage multiple dashboards and propagate changes to clones. Created a cloning feature across multiple instances of Superset to allow for a seamless transition between staging and production environments to reduce deployment time.`,
+      description: `I helped develop an open-source web application using Python, JavaScript, and React to replace a command line proof-of-concept in an agile team for over 100 UNICEF offices worldwide to save licensing costs. I implemented a cloud-based version control feature using Firebase and Flask to allow large teams to effectively manage multiple dashboards and propagate changes to clones. I created a cloning feature across multiple instances of Superset to allow for a seamless transition between staging and production environments to reduce deployment time.`,
       skills: ["Python", "Flask", "Firebase", "JavaScript", "React"],
     },
     {
@@ -23,7 +23,7 @@ const experienceData = [
       company: "University of Toronto - Deptartment of Statistics",
       dateRange: "May 2023 — August 2023",
       location: "Toronto, ON",
-      description: `Integrated Pandas and Statsmodels into Python coding and tutorial exercises to enhance the understanding of applications of data science techniques for over 600 students. Worked with the professor to coordinate collaborative efforts using Git and created comprehensive unit tests resulting in effective management of issues, code reviews, and updated lecture material.`,
+      description: `I integrated Pandas and Statsmodels into Python coding and tutorial exercises to enhance the understanding of applications of data science techniques for over 600 students. Also, I worked with the professor to coordinate efforts using Git and created comprehensive unit tests resulting in effective management of issues, code reviews, and updated lecture material.`,
       skills: ["Python", "NumPy", "Pandas", "Statsmodels"],
     },
   ];
--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,18 +1,27 @@
 import ScheduleImage from "../images/ScheduleImage.png";
 import PacManImage from "../images/PacManImage.png";
 import SoundSpaceImage from "../images/SoundSpaceImage.png";
+import clifyScreenshot from "../images/clifyScreenshot.png";
 
 const projectsData = [
     {
       id: 1,
+      title: "CLI-fy",
+      description: "Spotify statistics visualizer, view your top artists and songs in a terminal theme.",
+      image: clifyScreenshot,
+      repoLink: "https://clify-omega.vercel.app/",
+      skills: ['Next.js', 'React', 'JavaScript'],
+    },
+    {
+      id: 2,
       title: "Interactive Scheduling System",
-      description: "Interactive scheduling system for one-on-one meetings. Includes email reminders, setting meeting priority, and a calendar to track meetings. ",
+      description: "Interactive scheduling system for one-on-one meetings. Includes email reminders, setting meeting priority, and a calendar to track meetings.",
       image: ScheduleImage, 
       repoLink: "https://github.com/mistryrohan/Scheduling-System-UI",
       skills: ['React', 'JavaScript', 'Django', 'Python', 'SQLite'],
     },
     {
-      id: 2,
+      id: 3,
       title: "Image AI PacMan",
       description: "The original Pac-Man game reimagined using Cohere's APIs to add a story layer. Made at UofTHacks 11.",
       image: PacManImage,
@@ -20,7 +29,7 @@ const projectsData = [
       skills: ['Python', 'PyGame'],
     },
     {
-      id: 3,
+      id: 4,
       title: "UofT SoundSpace | Quiet Study Spaces Locator",
       description: "Mobile application for UofT students to discover quiet study spaces on campus. Made at UofTHacks 10.",
       image: SoundSpaceImage,
